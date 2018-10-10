6c43278b8bd2fa55de3acdddb7204857b5543fc5
Keda87 <adiyatmubarak@gmail.com>
2018-10-11 00:46:41 +0700

    Update UI

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from collections import OrderedDict
 from datetime import datetime
 
-from flask import Flask, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -10,15 +9,57 @@ app = Flask(__name__)
 def index():
     experience = (datetime.now() - datetime(year=2014, month=10, day=1)).days
     experience = int(experience / 360)
-    response = OrderedDict(
-        about=f'Software engineer with {experience} years experience, focused on Backend engineering.',
-        contact={
-            'email': 'adiyatmubarak@gmail.com',
-            'twitter': 'https://twitter.com/adiyatmubarak',
-            'linkedin': 'https://www.linkedin.com/in/adiyatmubarak/',
-            'github': 'https://github.com/Keda87',
-            'stackoverflow': 'https://stackoverflow.com/users/story/1936697',
-            'blog': 'https://adiyatmubarak.wordpress.com/'
+    list_experience = [
+        {
+            'year_info': 'Mar 2017-Present',
+            'company': '88Spares.com',
+            'role': 'Backend Engineer',
+            'description': 'Building backend services with python.',
         },
-    )
-    return jsonify(response)
+        {
+            'year_info': 'Jan 2016-Feb 2017',
+            'company': 'Oprent.com',
+            'role': 'Backend Engineer',
+            'description': ('Building web API, internal tools for analytic '
+                            'dashboard and third party integration to main '
+                            'platform.')
+        },
+        {
+            'year_info': 'Nov 2015-Jan 2016',
+            'company': 'Kargo.co.id',
+            'role': 'Backend & Android Developer',
+            'description': ('Provide MVP product for backend service and '
+                            'android client both for driver and customer.')
+        },
+        {
+            'year_info': 'Oct 2014-Jan 2016',
+            'company': 'PT. Polatic Informatika Indonesia',
+            'role': 'Software Engineer',
+            'description': ('Responsible to develop backend system, improvement,'
+                            ' optimization and bug fixing to a project from '
+                            'various clients.')
+        },
+        {
+            'year_info': 'Aug 2012-Jan 2014',
+            'company': 'Telkom University',
+            'role': 'Practicum Assistant',
+            'description': ('Assigned to be practicum assistant of Java Object '
+                            'Oriented Programming course on 3rd semester and '
+                            'assigned as Java EE practicum assistant on 5th '
+                            'semester.')
+        },
+        {
+            'year_info': 'May 2013-Jul 2013',
+            'company': 'PT. Neuronworks Indonesia',
+            'role': 'Intern',
+            'description': ('Tester and Bug fixing in business process and user '
+                            'interface on EOFFICE Online Memo web based '
+                            'application.')
+        }
+    ]
+
+    context = {
+        'experience': experience,
+        'list_experience': list_experience,
+    }
+    return render_template('civic/index.html', **context)
